12c7042520e307d6792354274576be615918051f
brings123 <fc_afraoucene@esi.dz>
2020-09-20 17:38:30 +0200

    Header component done!

---
+++ components/header.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Header(){
+    return (
+        <View style={styles.header}> 
+            <Text style={styles.title}>My Todos list</Text>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    header: {
+        height:70,
+        backgroundColor:'coral',
+        paddingVertical:8,
+        paddingHorizontal:6,
+    },
+    title: {
+        fontSize:20,
+        color:'#fff',
+        fontWeight:'bold',
+        textAlign:'center',
+        marginTop:20,
+    }
+});
